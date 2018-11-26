3e03063efa353c0e9ff35054ce9f6c9355cc170a
riccardo <riccardo@linx.net>
2018-11-26 08:56:07 +0000

    Add Convolutional NN classifier

--- classifiers/utils/preprocessing.py
+++ classifiers/utils/preprocessing.py
@@ -1,12 +1,17 @@
 """ Input preprocessing functions """
 
 
-def preprocess_raw_mnist_data(x):
+def preprocess_raw_mnist_data(x, conv=False):
     """ Preprocessing the data """
-    # Reshape x
+
+    # Change element type
     x = x.astype('float32')
 
     # Transform all input matrix elements in values belonging to [0,1] interval
     x /= 255
+
+    if conv:
+        # Reshape if convolutional (channel last)
+        x = x.reshape(x.shape[0], 28, 28, 1)
 
     return x
