e1317ef800dbd211f6ddeda4657a96d62dce5685
riccardo <riccardo@linx.net>
2018-12-14 03:33:37 +0000

    Fix typo

--- classifiers/semi_gan_classifier.py
+++ classifiers/semi_gan_classifier.py
@@ -46,7 +46,7 @@ def discriminator():
     return model
 
 
-def main(plot=False, train=False, epochs=5, unlabeled_size=5400):
+def main(plot=False, train=False, epochs=5, unlabelled_size=5400):
     """ Main function """
     # Get mnist train and test dataset
     (x_train, y_train), (x_test, y_test) = get_real_mnist()
@@ -65,17 +65,17 @@ def main(plot=False, train=False, epochs=5, unlabeled_size=5400):
 
     # Combine real and synthetic data
     label_size = 5400
-    ratio = label_size / (label_size + unlabeled_size)
+    ratio = label_size / (label_size + unlabelled_size)
 
     if train:
         # Train classifier
         print('\ntrain the classifier')
 
-        x_comb_train = np.append(x_gan_train[:unlabeled_size], x_train[:label_size], axis=0)
+        x_comb_train = np.append(x_gan_train[:unlabelled_size], x_train[:label_size], axis=0)
         y_comb_train = [
-            np.append(np.zeros(unlabeled_size), np.ones(label_size), axis=0),
-            # We assign the label 10 to all unlabeled examples as the digit that represents a fake image.
-            np.append(np.zeros(unlabeled_size)+10, y_train[:label_size], axis=0)
+            np.append(np.zeros(unlabelled_size), np.ones(label_size), axis=0),
+            # We assign the label 10 to all unlabelled examples as the digit that represents a fake image.
+            np.append(np.zeros(unlabelled_size)+10, y_train[:label_size], axis=0)
         ]
 
         discrim.fit(x_comb_train, y_comb_train, epochs=epochs)
@@ -131,7 +131,7 @@ if __name__ == '__main__':
                         default=5400,
                         choices=[5400, 0],
                         type=int,
-                        dest='unlabeled_size'
+                        dest='unlabelled_size'
                         )
     args = parser.parse_args()
-    main(train=args.train, epochs=args.epochs, unlabeled_size=args.unlabeled_size)
+    main(train=args.train, epochs=args.epochs, unlabelled_size=args.unlabelled_size)
