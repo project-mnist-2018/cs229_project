5f92b099bf21a680d44de41b7a1501822189c5d3
riccardo <riccardo@linx.net>
2018-12-14 03:31:19 +0000

    Add argparse to semi-gan

--- classifiers/semi_gan_classifier.py
+++ classifiers/semi_gan_classifier.py
@@ -1,4 +1,5 @@
 """ Modified GAN semi-supervised learning MNIST classifier """
+import argparse
 from utils.misc import get_real_mnist, get_gan_mnist, plot_mist
 from utils.preprocessing import preprocess_raw_mnist_data
 import tensorflow as tf
@@ -45,7 +46,7 @@ def discriminator():
     return model
 
 
-def main(plot=False, train=False):
+def main(plot=False, train=False, epochs=5, unlabeled_size=5400):
     """ Main function """
     # Get mnist train and test dataset
     (x_train, y_train), (x_test, y_test) = get_real_mnist()
@@ -59,16 +60,11 @@ def main(plot=False, train=False):
     x_test = preprocess_raw_mnist_data(x_test, conv=True)
     x_gan_train = preprocess_raw_mnist_data(x_gan_train, conv=True)
 
-
     # Build modified discriminator
     discrim = discriminator()
 
-
-    epochs = 100
-
     # Combine real and synthetic data
     label_size = 5400
-    unlabeled_size = 5400
     ratio = label_size / (label_size + unlabeled_size)
 
     if train:
@@ -111,4 +107,31 @@ def main(plot=False, train=False):
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", "-t", help="It specifies if you want to train the model first. (Default False)",
+                        action="store_true",
+                        dest='train')
+    parser.add_argument("--epochs",
+                        "-e",
+                        help=(
+                            "It specifies how many epochs to use for training the "
+                            "model or which corresponding save weights to use. (Default 5)"
+                        ),
+                        default='5',
+                        type=int,
+                        dest='epochs'
+                        )
+    parser.add_argument("--unlabelled_size",
+                        "-u",
+                        help=(
+                            "It allows you to specify how many unlabelled "
+                            "samples to run the training with. (Default 5400)."
+                            "N.B: Use it in combination with Training: -t"
+                        ),
+                        default=5400,
+                        choices=[5400, 0],
+                        type=int,
+                        dest='unlabeled_size'
+                        )
+    args = parser.parse_args()
+    main(train=args.train, epochs=args.epochs, unlabeled_size=args.unlabeled_size)
